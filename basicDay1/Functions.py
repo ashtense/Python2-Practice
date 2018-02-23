4553d2988fac5cde12dc594e524901413a045938
Ashwani Solanki <ashwanisolanki@Ashwanis-MacBook-Air.local>
2018-02-23 18:15:00 +0530

    Function check

--- basicDay1/Functions.py
+++ basicDay1/Functions.py
@@ -4,14 +4,14 @@ Created on Oct 11, 2017
 @author: solanka
 '''
 def print_two(*args):
-    arg1, arg2, arg3 = args
+    arg1, arg2, arg3, arg4 = args
     print "arg1: %s, arg2: %s" % (arg1, arg2)
-    print "arg3"
+    print "%s lol: %s" %(arg3,arg4)
 # Indentation is really important here. 4 spaces right from left should be the indentation on an instruction for it to be a part of function.
      
 def print_two_java_way(arg1, arg2):
     print "arg1: %s, arg2: %s" % (arg1, arg2)
     
     
-print_two("Ashwani", "Solanki LOL", "Geronimo")
+print_two("Ashwani", "Solanki LOL", "Geronimo", "Hello")
 print_two_java_way("Chaudhary", "Singh")
