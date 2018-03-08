1091dc7bb9b1a1bfcc23056c28e1920be2f4565a
Ashwani Solanki <Ashwani.solanki@ADP.com>
2018-03-08 15:16:31 +0530

    Code quality fixe

--- Logical/Logical1.py
+++ Logical/Logical1.py
@@ -3,15 +3,15 @@ cats = 20
 dogs = 15
 
 if people > dogs:
-		print "Hello everyone"
+    print "Hello everyone"
 
 if people == cats:
-		print "Hello cats are nearly as much as humans these days."
+    print "Hello cats are nearly as much as humans these days."
 
 if dogs < cats:
-		print "dogs can do without cats."
+    print "dogs can do without cats."
 
 dogs += 7
 
 if people < dogs:
-		print "Good dogs"
+    print "Good dogs"
--- Logical/Logical2.py
+++ Logical/Logical2.py
@@ -2,27 +2,27 @@ people = 30
 cars = 40
 buses = 15
 
-if cars>people:
-	print "More cars then people."
-elif cars<people:
-	print "Less cars then people."
+if cars > people:
+    print "More cars then people."
+elif cars < people:
+    print "Less cars then people."
 
 
 cars = 25
 
 
-if cars>people:
-	print "More cars then people."
-elif cars<people:
-	print "Less cars then people."
+if cars > people:
+    print "More cars then people."
+elif cars < people:
+    print "Less cars then people."
 
 
 cars = 30
 
 
-if cars>people:
-	print "More cars then people."
-elif cars<people:
-	print "Less cars then people."
+if cars > people:
+    print "More cars then people."
+elif cars < people:
+    print "Less cars then people."
 else:
-	print "Can't decide"
+    print "Can't decide"
