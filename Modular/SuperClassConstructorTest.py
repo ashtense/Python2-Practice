9d545e865fa318fdc8aca8fde6a7b5e08e406fca
Ashwani Solanki <solanki.ashwani@yahoo.com>
2018-05-15 21:19:30 +0530

    First experiments with super constructore

---
+++ Modular/SuperClassConstructorTest.py
@@ -0,0 +1,24 @@
+class Parent_class(object):
+
+    def __init__(self, parent_class_variable):
+        self.parent_class_incomming_variable = parent_class_variable
+        print self.parent_class_incomming_variable
+
+    def experimental_method():
+        print "Inside the experimental method in parent class"
+
+
+class Child_class(Parent_class):
+
+    def __init__(self):
+        print "First you're inside the child class"
+        parent_class_variable = "Hello darling, my first experiment \
+with python constructors"
+        super(Child_class, self).__init__(parent_class_variable)
+        print "This is the end of the child class"
+        # below piece of code doesn't work. maybe my understanding is\
+        # a bit wrong. lets see if the book provides such a solution
+        super(Child_class, self).experimental_method
+
+
+thing = Child_class()
