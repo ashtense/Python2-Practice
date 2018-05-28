154096ea35b0e18808a7005126ac318281b5e61d
Ashwani Solanki <ashwani.solanki@ADP.com>
2018-05-28 16:59:47 +0530

    Changes

--- Modular/Classes/Parent2.py
+++ Modular/Classes/Parent2.py
@@ -1,10 +1,17 @@
 class Parent(object):
 
+    def __init__(self):
+        print "Parent method's constructor"
+
     def altered(self):
         print "Parent altered"
 
 
 class Child(Parent):
+
+    def __init__(self):
+        print "Child method's constructor"
+        super(Child, self).__init__()
 
     def altered(self):
         print "Child altered"
