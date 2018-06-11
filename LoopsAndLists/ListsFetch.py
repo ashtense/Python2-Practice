d727c5963436470589c2542aa5c49b77da43a83e
Ashwani Solanki <ashwani.solanki@ADP.com>
2018-06-11 10:36:18 +0530

    New list features

--- LoopsAndLists/ListsFetch.py
+++ LoopsAndLists/ListsFetch.py
@@ -2,3 +2,13 @@ animals = ['bear', 'dog', 'elephant', 'whales', 'dinosaurs']
 print "First animal is %s" % animals[0]
 
 print "4th animal is %s" % animals[3]
+
+# here we print subset of a list. It excludes item at \
+# the second position given. weird
+print animals[1:3]
+
+to_do = ['run', 'cleanup']
+# lists inside a list is also permitted. neat
+final_list = [to_do, animals]
+
+print final_list
